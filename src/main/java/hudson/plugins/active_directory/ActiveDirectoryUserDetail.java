4ea46fae7b9e44a893dfb6636a4b7b955f690c6a
kohsuke <kohsuke@dev.java.net>
2008-02-27 16:08:54 +0000

    fixing IllegalArgumentException (#1229)

--- src/main/java/hudson/plugins/active_directory/ActiveDirectoryUserDetail.java
+++ src/main/java/hudson/plugins/active_directory/ActiveDirectoryUserDetail.java
@@ -8,6 +8,8 @@ import org.acegisecurity.userdetails.User;
  */
 public class ActiveDirectoryUserDetail extends User {
     public ActiveDirectoryUserDetail(String username, String password, boolean enabled, boolean accountNonExpired, boolean credentialsNonExpired, boolean accountNonLocked, GrantedAuthority[] authorities) throws IllegalArgumentException {
-        super(username, password, enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
+        // Acegi doesn't like null password, but during remember-me processing we don't know the password.
+        // so we need to set some dummy. See #1229
+        super(username, password!=null?password:"PASSWORD", enabled, accountNonExpired, credentialsNonExpired, accountNonLocked, authorities);
     }
 }
