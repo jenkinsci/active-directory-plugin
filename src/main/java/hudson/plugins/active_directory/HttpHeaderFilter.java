f2419ccad40b037594cd56a479d6a2c8e4e6b163
Michael Sprauer <Michael.Sprauer@sap.com>
2019-09-02 16:10:14 +0200

    Support for "Authentication: basic" header
    Signed-off-by: Michael Sprauer <Michael.Sprauer@sap.com>

--- src/main/java/hudson/plugins/active_directory/HttpHeaderFilter.java
+++ src/main/java/hudson/plugins/active_directory/HttpHeaderFilter.java
@@ -1,6 +1,9 @@
 package hudson.plugins.active_directory;
 
+import hudson.model.User;
+import hudson.util.Scrambler;
 import jenkins.model.Jenkins;
+import jenkins.security.ApiTokenProperty;
 import org.acegisecurity.Authentication;
 import org.acegisecurity.GrantedAuthority;
 import org.acegisecurity.context.SecurityContextHolder;
@@ -23,28 +26,64 @@ class HttpHeaderFilter implements Filter {
         this.activeDirectorySecurityRealm = activeDirectorySecurityRealm;
     }
 
-    public void doFilter(ServletRequest request,
+    public void doFilter(ServletRequest servletRequest,
                          ServletResponse response, FilterChain chain)
             throws IOException, ServletException {
-        HttpServletRequest r = (HttpServletRequest) request;
-
-        String userFromHeader;
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
         Authentication auth = Jenkins.ANONYMOUS;
-        if ((getUserHeader() != null && (userFromHeader = r.getHeader(getUserHeader())) != null)) {
-            LOGGER.log(Level.FINE, "User from HTTP Header: {0}", userFromHeader);
+        String authenticatedUserFromApiToken = getUserFromAuthorizationHeader(request);
 
+        String userName = authenticatedUserFromApiToken == null ? getUserFromReverseProxyHeader(request) : authenticatedUserFromApiToken;
+        if (userName != null) {
             try {
-                UserDetails userDetails = activeDirectorySecurityRealm.getAuthenticationProvider().loadUserByUsername(userFromHeader);
+                UserDetails userDetails = activeDirectorySecurityRealm.getAuthenticationProvider().loadUserByUsername(userName);
 
                 GrantedAuthority[] authorities = userDetails.getAuthorities();
 
-                auth = new UsernamePasswordAuthenticationToken(userFromHeader, "", authorities);
+                auth = new UsernamePasswordAuthenticationToken(userName, "", authorities);
             } catch (UsernameNotFoundException e) {
-                LOGGER.log(Level.FINE, "User from HTTP Header {0} not found in LDAP", userFromHeader);
+                LOGGER.log(Level.FINE, "User from HTTP Header {0} not found in LDAP", userName);
             }
         }
+
         SecurityContextHolder.getContext().setAuthentication(auth);
-        chain.doFilter(r, response);
+        chain.doFilter(request, response);
+    }
+
+    private String getUserFromAuthorizationHeader(HttpServletRequest request) {
+        String authorization;
+        if ((authorization = request.getHeader("Authorization")) != null && authorization.toLowerCase().startsWith("basic ")) {
+            String uidpassword = Scrambler.descramble(authorization.substring(6));
+            int idx = uidpassword.indexOf(':');
+            if (idx >= 0) {
+                String username = uidpassword.substring(0, idx);
+                String password = uidpassword.substring(idx + 1);
+
+                // attempt to authenticate as API token
+                User u = User.get(username, false);
+                if (u != null) {
+                    ApiTokenProperty t = u.getProperty(ApiTokenProperty.class);
+                    if (t != null && t.matchesPassword(password)) { // Authenticate API-Token
+                        return username;
+                    } else { // Authenticate against LDAP
+                        UsernamePasswordAuthenticationToken auth = new UsernamePasswordAuthenticationToken(username, password);
+                        UserDetails details = activeDirectorySecurityRealm.getAuthenticationProvider().retrieveUser(username, auth);
+                        if (details != null)
+                            return username;
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    private String getUserFromReverseProxyHeader(HttpServletRequest request) {
+        String userFromHeader;
+        if ((getUserHeader() != null && (userFromHeader = request.getHeader(getUserHeader())) != null)) {
+            LOGGER.log(Level.FINE, "User from HTTP Header: {0}", userFromHeader);
+            return userFromHeader;
+        }
+        return null;
     }
 
     private String getUserHeader() {
