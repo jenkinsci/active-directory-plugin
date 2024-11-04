1c3efa6146d1212ef8833f3cd9bf028c0298c72d
Yaroslav <91559310+yaroslavafenkin@users.noreply.github.com>
2024-11-04 21:30:18 +0200

    [JENKINS-74035] Extract inline script from `ActiveDirectorySecurityRealm/configAdvanced.jelly` (#210)

---
+++ src/main/resources/hudson/plugins/active_directory/ActiveDirectorySecurityRealm/ad-turn-off-autocomplete.js
@@ -0,0 +1,4 @@
+window.addEventListener("DOMContentLoaded", function(event) {
+    document.getElementsByName("_.bindPassword")[0].autocomplete = "off";
+    document.getElementsByName("_.bindName")[0].autocomplete = "off";
+});
