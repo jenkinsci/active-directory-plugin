5028dea8e0acc45ff9d6be302d4e1cbc1f9da464
mike cirioli <mikecirioli@gmail.com>
2026-06-11 15:34:42 -0400

    SECURITY-3651
    SECURITY-3651
    
    SECURITY-3651

--- src/test/java/hudson/plugins/active_directory/ActiveDirectoryAuthenticationProviderTest.java
+++ src/test/java/hudson/plugins/active_directory/ActiveDirectoryAuthenticationProviderTest.java
@@ -14,4 +14,28 @@ public class ActiveDirectoryAuthenticationProviderTest {
         assertEquals("LDAP://it\\/ops", dnToLdapUrl("it/ops"));
         assertEquals("LDAP://foo\\#\\,bar", dnToLdapUrl("foo\\#\\,bar"));
     }
+
+    @Test
+    public void testEscapeLdapFilterValue() {
+        // RFC 4515 metacharacters
+        assertEquals("\\2a", escapeLdapFilterValue("*"));
+        assertEquals("\\28", escapeLdapFilterValue("("));
+        assertEquals("\\29", escapeLdapFilterValue(")"));
+        assertEquals("\\5c", escapeLdapFilterValue("\\"));
+        assertEquals("\\00", escapeLdapFilterValue("\0"));
+
+        // Combined injection attempts - pipe doesn't need escaping, only parens and wildcard
+        assertEquals("admin\\29\\28|\\28sAMAccountName=\\2a",
+                     escapeLdapFilterValue("admin)(|(sAMAccountName=*"));
+        assertEquals("\\2a", escapeLdapFilterValue("*"));
+
+        // Normal usernames should pass through
+        assertEquals("validUser", escapeLdapFilterValue("validUser"));
+        assertEquals("user.name", escapeLdapFilterValue("user.name"));
+        assertEquals("user@domain", escapeLdapFilterValue("user@domain"));
+
+        // Edge cases
+        assertEquals("", escapeLdapFilterValue(null));
+        assertEquals("", escapeLdapFilterValue(""));
+    }
 }
